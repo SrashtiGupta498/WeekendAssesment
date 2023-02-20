977219591aead951ea7503710e36914452f73ea0
srashtiGupta <srashtig1009@gmail.com>
2023-02-20 18:11:02 +0530

    commit message

---
+++ DOWNLOADS/Angular_Workspace/Angulardemo/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inventory-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Basics of Typescript for Angular';
+   data=90
+  // data:number|string =20
+  obj :{name:string ,phn:number}={name:'string' ,phn:123456}
+  arr:number[]=[20,30,40]
+  getData(item:any)
+   {
+      //this.data="hello"
+    this.obj={name:"Radha",phn:1234}
+    this.arr=[10,20,30];
+      return item;
+   }
+  getItem()
+  {
+  this.getData(80)
+}
+}
